89e778d18490c9ae81950ceef66e36ea09386275
Nicola Lerra <github.nicolalerra@gmail.com>
2022-10-05 17:40:26 +0200

    modifiche varie

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// cliccare sul primo bottone e creare un colore random di background
+
+function colore() {
+  let bgRandom = document.querySelector("body");
+  bgRandom.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)}`;
+}
+
+// PRIMA OPZIONE
+function image() {
+  let imgGenerate = document.querySelector(".image");
+  imgGenerate.innerHTML += `<img src="https://picsum.photos/200/300/?random&t=${new Date().getTime()}">`;
+}
+
+// SECONDA OPZIONE
+
+// let arrayImage = ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"];
